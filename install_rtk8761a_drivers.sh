53ae1e1882c05d6d7532360d1243b942c2e94459
zi5t0 <zbotpath@gmail.com>
2020-12-10 12:45:29 +0100

    Add files via upload

---
+++ install_rtk8761a_drivers.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+# Installation for Ubuntu
+apt install make gcc build-essential bluez wget
+# Download file to /tmp
+wget 'https://github.com/zi5t0/drivers/raw/main/drivers_rtk8761a.zip' -O /tmp/drivers_rtk8761a.zip
+# Give permissions to downloaded zip
+chmod 777 /tmp/drivers_rtk8761a.zip
+# Decompress drivers
+unzip /tmp/drivers_rtk8761a.zip -d /tmp/drivers_rtk8761a
+# Remove temporal zip
+rm /tmp/drivers_rtk8761a.zip
+# Go to uart module and compile it
+cd /tmp/drivers_rtk8761a/uart && sudo make install
+# Go to usb module and compile it
+cd /tmp/drivers_rtk8761a/usb && sudo make install
+# Go to general module and compile it
+cd /tmp/drivers_rtk8761a/ && sudo make install INTERFACE=all
+# Copy driver files to ubuntu folder
+sudo cp -R /tmp/drivers_rtk8761a/rtkbt-firmware/lib/firmware/rtlbt/* /lib/firmware/rtl_bt/
+# Remove temporal folder
+sudo rm -r /tmp/drivers_rtk8761a
+# Successfully drivers installed message
+echo "Congratulations! Drivers rtk_8761a successfully installed"
+
